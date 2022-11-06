bb8a9744278964d9e3897b7bd02dd8fb70154427
pppponny <88283757+pppponny@users.noreply.github.com>
2022-11-06 22:52:55 +0700

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include_once('Jommoto02.html');?>
-<?php include_once('page_helmet.html');?>
-<?php include_once('page_racingsuit.html');?>
-<?php include_once('page_Suspension.html');?>
-<?php include_once('page_Exhuaust.html');?>
-<?php include_once('page_Chain.html');?>
