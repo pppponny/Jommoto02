7421b95368ee5b4d6de62cb69f627d4b6534f684
jomchai09 <116899783+jomchai09@users.noreply.github.com>
2022-11-06 21:45:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');             
+const path = require('path');                 
+const app = express();                        
+const PORT = process.env.PORT || 3000;             
+app.use(express.static(__dirname));
+
+app.use('/', function (req, res, next) {        
+    var options = {
+        root: path.join(__dirname)            
+    };
+    var fileName = 'Jommoto02.html';               
+    res.sendFile(fileName, options, function (err) { 
+        if (err) {                        
+            next(err);
+        } else {
+            console.log('Sent:', fileName);        
+            next();
+        }
+    });
+});
+
+app.listen(PORT, function (err) {                
+    if (err) console.log(err);                
+    console.log("Server listening on PORT", PORT);    
+});
+
+app.get('/', function (req, res) {            
+    console.log("File Sent")                    
+    res.send();
+});
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once('Jommoto02.html');?>
+<?php include_once('page_helmet.html');?>
+<?php include_once('page_racingsuit.html');?>
+<?php include_once('page_Suspension.html');?>
+<?php include_once('page_Exhuaust.html');?>
+<?php include_once('page_Chain.html');?>
